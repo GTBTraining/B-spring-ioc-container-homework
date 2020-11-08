494b109f0294406db43f9f18e9aac3685d3801c1
“pyqpyqpyq” <alwayswannaflyx@gmail.com>
2020-11-08 13:47:49 +0800

    [pyq] attempt to finish p2s2

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
@@ -1,10 +1,9 @@
 package com.thoughtworks.capability.demospringioccontainer;
-
 import org.springframework.stereotype.Service;
 
 @Service
 public class GreetingService {
-
+    System.out.println("-------------initing GreetingService");
     String sayHi() {
         return "hello world";
     }
