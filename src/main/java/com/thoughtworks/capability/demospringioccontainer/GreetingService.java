8c3c7c1b0e2d63c15a315b471c45894bce96a971
“pyqpyqpyq” <alwayswannaflyx@gmail.com>
2020-11-08 13:22:39 +0800

    [pyq] implement the first attempt to task 2

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
@@ -1,13 +1,14 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Service;
 
 import static org.springframework.beans.factory.config.ConfigurableBeanFactory.SCOPE_PROTOTYPE;
 import static org.springframework.beans.factory.config.ConfigurableBeanFactory.SCOPE_SINGLETON;
 
 @Service
-//@Scope(SCOPE_PROTOTYPE)
+@Scope(value = "prototype",proxyMode = ScopedProxyMode.TARGET_CLASS)
 //@Scope(SCOPE_SINGLETON)
 public class GreetingService {
     public GreetingService(){
