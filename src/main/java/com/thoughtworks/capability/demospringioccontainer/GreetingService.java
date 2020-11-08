a846a562dea3a38d4e3854de33d6d7219949b949
“pyqpyqpyq” <alwayswannaflyx@gmail.com>
2020-11-08 13:10:02 +0800

    [pyq] add system to test

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
@@ -1,10 +1,18 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
-@Service
-public class GreetingService {
+import static org.springframework.beans.factory.config.ConfigurableBeanFactory.SCOPE_PROTOTYPE;
+import static org.springframework.beans.factory.config.ConfigurableBeanFactory.SCOPE_SINGLETON;
 
+@Service
+//@Scope(SCOPE_PROTOTYPE)
+//@Scope(SCOPE_SINGLETON)
+public class GreetingService {
+    public GreetingService(){
+        System.out.println("++++++++++Greeting service is initing");
+    }
     String sayHi() {
         return "hello world";
     }
